868c98dba76ab024cecccc6794497f5e289604d8
Arti199424 <arti48589@gmail.com>
2022-12-24 10:54:13 -0800

    searchpage

--- swiggyClone/script.js
+++ swiggyClone/script.js
@@ -7,10 +7,10 @@ const left=document.getElementById('left');
 
 function myfunction(){
     sidebar.style.width="30rem";
-    left.style.color="0rem";
+    // left.style.color="0rem";
 }
 
 function closebtn(){
     sidebar.style.width="0rem";
-    left.style.height="4rem";
+    // left.style.height="4rem";
 }
