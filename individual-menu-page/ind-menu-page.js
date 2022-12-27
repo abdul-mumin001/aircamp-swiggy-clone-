a28657b6d6e1cb9c1d2e932b98258eed3946e1cf
unknown <myselfbhupesh00@gmail.com>
2022-12-27 16:23:58 +0530

    menu_item_page

---
+++ individual-menu-page/ind-menu-page.js
@@ -0,0 +1,59 @@
+const category = [
+    "Recommended",
+    "Vegetable Veg Pag Pizza",
+    "Best In Pizza Veg",
+    "Chicken Pizza (Pan Pizza)",
+    "Cheese Stuffed Crust Pizza",
+    "Vegetable Starters",
+    "Chicken Starters",
+    "Desserts",
+    "End of List"
+]
+
+const menu_page = document.querySelector(".menu-page");
+
+
+// Categories Section Menu item
+const menu__categories = document.createElement("div");
+menu__categories.className = "menu__categories";
+
+const add_menu_categories = (parent, category) => {
+    category.forEach(menu => {
+        const para = document.createElement("p");
+        para.textContent = menu;
+
+        parent.appendChild(para);
+    })
+    
+    menu_page.appendChild(menu__categories);
+    // const menu__item__page = document.querySelector(".menu__item__page");
+
+}
+
+add_menu_categories(menu__categories, category);
+
+// ***************************************Priya's Part *************************************************
+
+const plus = document.querySelector(".plus");
+const minus = document.querySelector(".minus");
+const number = document.querySelector(".number");
+const count = document.querySelector(".item-count");
+
+plus.addEventListener("click", () => {
+    
+  number.innerText++;
+
+  count.innerText++;
+
+  // number.value("")
+});
+minus.addEventListener("click", () => {
+    if(number.innerText == 0){
+        return
+    }
+  number.innerText--;
+
+  count.innerText--;
+});
+
+// End of Priya part
