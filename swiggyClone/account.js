00da82234fbe6bf7f0e9e8f254353afe8de597c0
Arti199424 <arti48589@gmail.com>
2022-12-28 15:30:38 -0800

    editaccount

--- swiggyClone/account.js
+++ swiggyClone/account.js
@@ -1,34 +1,36 @@
-// let nav2 = document.getElementById("user-page-main-content-header");
-// let container = document.getElementById("user-page-inner-content");
-// let header1 =document.getElementById("navbar");
+let nav2 = document.getElementById("user-page-main-content-header");
+let container = document.getElementById("user-page-inner-content");
+let header1 =document.getElementById("navbar");
 
-// document.addEventListener('scroll', (e) => {
-//     let axis = container.getBoundingClientRect().top;
-//     console.log(axis);
-//     if(axis < 0) {
-//         nav2.style.position = "fixed";
-//         header1.style.position = "static"
-//     } else {
-//         nav2.style.position = "static";
-//         header1.style.position = "fixed"
-//         nav2.style.backgroundColor="#003a55d0"
-//         nav2.style.width="90em"
-//         nav2.style.height="2em"
-//         nav2.style.marginBottom="1em"
-//         container.style.width="100%"
-//         container.style.margin="display:none"
+document.addEventListener('scroll', (e) => {
+    let axis = container.getBoundingClientRect().top;
+    console.log(axis);
+    if(axis < 0) {
+        nav2.style.position = "fixed";
+        header1.style.opacity = "0"
+    } else {
+        nav2.style.position = "block";
+        header1.style.opacity = "1"
+        nav2.style.backgroundColor="#003a55"
+        nav2.style.width="90em"
+        nav2.style.height="2.5em"
+        nav2.style.marginBottom="1em"
+        container.style.width="100%"
+        container.style.padding="display:none"
+        nav2.style.paddingTop="0em"
+        nav2.style.paddingLeft="2em"
 
-//     }
-// })
+    }
+})
 
-var scrollTrigger = 60;
+// var scrollTrigger = 60;
 
-window.onscroll = function() {
-  // We add pageYOffset for compatibility with IE.
-  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
-    console.log("OK")
-    document.querySelector(".user-page-main-content-header").classList.add("on-scroll");
-  } else {
-  document.querySelector(".user-page-main-content-header").classList.remove("on-scroll");
-  }
-};
+// window.onscroll = function() {
+//   // We add pageYOffset for compatibility with IE.
+//   if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+//     console.log("OK")
+//     document.querySelector(".user-page-main-content-header").classList.add("on-scroll");
+//   } else {
+//   document.querySelector(".user-page-main-content-header").classList.remove("on-scroll");
+//   }
+// };
